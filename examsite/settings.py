f7a2413e7f40ac3b62842ff5acbc3b1c980b93e6
masterpiece015v <45834706+masterpiece015v@users.noreply.github.com>
2019-07-31 09:01:00 +0900

    190731003

--- examsite/settings.py
+++ examsite/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -128,9 +127,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL='/exam/static/'
+STATIC_URL='/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR , 'exam' , 'media')
-STATIC_ROOT = os.path.join( BASE_DIR,'exam' , 'static')
+STATIC_ROOT = os.path.join( BASE_DIR,'static')
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
