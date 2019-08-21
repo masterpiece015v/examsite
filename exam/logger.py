834801a17182d7d3fe6fc13f05179a1bd5b7aaf3
masterpiece015v <45834706+masterpiece015v@users.noreply.github.com>
2019-08-21 18:02:09 +0900

    190731015

--- exam/logger.py
+++ exam/logger.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 import os
 
-path = os.path.join(settings.BASE_DIR,'django.log')
+path = os.path.join(settings.BASE_DIR,'log','django.log')
 
 def log_write( text ):
     with open( path , mode='a') as f:
