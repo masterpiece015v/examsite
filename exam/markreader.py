2f0181362bec2dea9916a5446d7160205dbf573a
masterpiece015v <45834706+masterpiece015v@users.noreply.github.com>
2019-08-22 12:58:05 +0900

    190731015

--- exam/markreader.py
+++ exam/markreader.py
@@ -12,14 +12,14 @@ PATH= os.path.join( settings.BASE_DIR , "exam")
 STATIC_PATH = os.path.join(settings.STATIC_ROOT, "exam", "data")
 
 class Ainum:
-    sess = tf.InteractiveSession()
+    sess = tf.compat.v1.InteractiveSession()
     #入力データ
-    x = tf.placeholder("float",shape=[None,784])
+    x = tf.compat.v1.placeholder("float",shape=[None,784])
     #正解データ
-    y_ = tf.placeholder( "float" ,shape=[None,10])
+    y_ = tf.compat.v1.placeholder( "float" ,shape=[None,10])
 
     def weight_variable( shape ):
-        initial = tf.truncated_normal( shape,stddev=0.1)
+        initial = tf.random.truncated_normal( shape,stddev=0.1)
         return tf.Variable(initial)
 
     def bias_variable( shape ):
@@ -30,7 +30,7 @@ class Ainum:
         return tf.nn.conv2d( x , W , strides=[1,1,1,1], padding='SAME')
 
     def max_pool_2x2( x ):
-        return tf.nn.max_pool( x, ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
+        return tf.nn.max_pool2d( x, ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
 
     W_conv1 = weight_variable([5,5,1,32])
     b_conv1 = bias_variable([32])
