1ca030f3d1a5d535064fe5055595ca2576d61b75
masterpiece015v <45834706+masterpiece015v@users.noreply.github.com>
2019-09-12 13:08:21 +0900

    dev02_012

--- exam/urls.py
+++ exam/urls.py
@@ -38,5 +38,5 @@ urlpatterns = [
 from django.conf import settings
 from django.conf.urls.static import static
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+#if settings.DEBUG:
+#    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
